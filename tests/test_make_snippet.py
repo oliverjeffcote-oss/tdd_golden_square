5bb4bc53984fd251920606e2ee33abdd65cb288d
Ollie Jeffcote <oliverjeffcote@gmail.com>
2025-10-07 17:10:07 +0100

    Exception added for None input

--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,4 +1,5 @@
 from lib.make_snippet import *
+import pytest
 
 def test_returns_full_sentence_given_less_than_5_words():
     result = make_snippet("Let's go party")
@@ -19,3 +20,10 @@ def test_long_sentences_are_truncated_to_5_words():
 def test_empty_string_is_returned_empty():
     result = make_snippet("")
     assert result == ""
+
+def test_none_input_throws_error():
+    
+    with pytest.raises(Exception) as e:
+        make_snippet(None)
+    error_message = str(e.value)
+    assert error_message == "Error: No input given."
